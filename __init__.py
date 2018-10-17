f4b0404895adb694745054e2b64a01d38b26e6c0
Christopher Berglund <13christopherb@gmail.com>
2018-10-17 11:05:24 -0700

    change

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from flask_heroku import Heroku
 """Initialization and configuration"""
 app = Flask(__name__)
 app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql-corrugated-33739'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/animals'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 heroku = Heroku(app)
 db = SQLAlchemy(app)
