d136467b3aedd56302ec04c5ebaddbb2664878b8
Christopher Berglund <13christopherb@gmail.com>
2018-10-17 14:54:43 -0700

    Add API support for animal locations

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from flask_heroku import Heroku
 """Initialization and configuration"""
 app = Flask(__name__)
 app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/animals'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jdjrchsjifapjp:cad088a14a2dd90e75b3ae71f13bfa4a43c154e70257bd5fd42b800542c17c60@ec2-23-23-80-20.compute-1.amazonaws.com:5432/dadelfp0dkv1uc'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 heroku = Heroku(app)
 db = SQLAlchemy(app)
